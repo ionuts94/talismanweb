a94493cd3e8ed9b695bf55656db4d2a23d47d1cb
Ionut <ionut.s_94@yahoo.com>
2021-12-27 00:11:27 +0200

    Created register function

---
+++ src/actions/auth.js
@@ -0,0 +1,36 @@
+export const register = async ({ username, password, confirmPassword }) => {
+    const serverResponse = await fetch('http://localhost:4000/register', {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+          'Content-Type': 'application/json'
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify({
+            username: username,
+            password: password,
+            confirmPassword: confirmPassword
+        }) // body data type must match "Content-Type" header
+    });
+    console.log(serverResponse.json())
+}
+
+export const validateData = ({ username, password, confirmPassword }) => {
+    let errorString = '';
+    if (username.length < 4) {
+        errorString += 'Username must be at least 4 characters long \n';
+    }
+    if (password.length < 5) {
+        errorString += 'Password must be at least 5 characters long \n';
+    }
+    if (password !== confirmPassword) {
+        errorString += "Passwords don't match \n";
+    }
+    if (!errorString) {
+        return true;
+    } else {
+        return errorString
+    }
+}
--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,16 +1,39 @@
-import React from 'react';
-import NavMenu from '../../components/NavMenu/NavMenu';
+import React, { useState } from 'react';
+import { register, validateData } from '../../actions/auth';
 import './RegisterPage.css';
 
 const RegisterPage = () => {
+    const [userData, setUserData] = useState({
+        username: '',
+        password: '',
+        confirmPassword: ''
+    });
+
+    const handleInputChange = (event) => {
+        setUserData({
+            ...userData,
+            [event.target.name]: event.target.value 
+        })
+    }
+    
+    const handleRegister = async () => {
+        const validate = validateData(userData);
+        if (validate === true) {
+           const response = await register(userData);
+           console.log(response);
+        } else {
+            alert(validate);
+        }
+    }
+
     return (
         <div className='register-page-container'>
             <div className='fake-nav'></div>
             <div className='register-form-container'>
-                <input type='text' placeholder='Username' />
-                <input type='password' placeholder='Password' />
-                <input type='password' placeholder='Confirm Password' />
-                <h1>SUBMIT</h1>
+                <input onChange={handleInputChange} value={userData.username} name='username' type='text' placeholder='Username' />
+                <input onChange={handleInputChange} value={userData.password} name='password' type='password' placeholder='Password' />
+                <input onChange={handleInputChange} value={userData.confirmPassword} name='confirmPassword' type='password' placeholder='Confirm Password' />
+                <h1 onClick={handleRegister}>SUBMIT</h1>
             </div>
         </div>
     )
