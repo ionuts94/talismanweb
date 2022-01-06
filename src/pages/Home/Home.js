21691dfff4bbc6ab526d13c6d66a2a5816087423
Ionut <ionut.s_94@yahoo.com>
2022-01-06 21:16:59 +0200

    Added some styling to 'details' section

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,48 +7,50 @@ const Home = () => {
 		<div className="home-container">
 			<div className='fake-nav'></div>
 			<div className='server-details-container'>
-				<h1>Server details:</h1>
-				<ul>
-					<li>
-						<span>Max level: 75</span>
-						<ul>
-							<li><span>Instant lvl 61</span></li>
-							<li><span>Fast lvl 61-70</span></li>
-							<li><span>Fair medium lvl 70-75</span></li>
-						</ul>
-					</li>
-					<li>
-						<span>Max pet level: 25</span>
-						<ul>
-							<li><span>One level per minute</span></li>
-						</ul>
-					</li>
-					<li>
-						<span>Starter items pack</span>
-						<ul>
-							<li><span>Contains 100 bound gold, blue set level 50-60 +7, 
-								<br />enhanced weapons 60-70 +7, enhanced asistants</span></li>
-						</ul>
-					</li>
-					<li>
-						<span>Max color drop: golden</span>
-						<ul>
-							<li><span>Dark golden magic bead drop. (25% success rate)</span></li>
-						</ul>
-					</li>
-					<li>
-						<span>Fair larges and pills drop</span>
-						<ul>
-							<li><span>4-6 larges/pills in 100 mobs</span></li>
-						</ul>
-					</li>
-					<li>
-						<span>No EZ, QL, PB to avoid apprentices.</span>
-					</li>
-					<li>
-						<span>No PRC sets just yet</span>
-					</li>
-				</ul>
+				<div>
+					<h1>Server details:</h1>
+					<ul>
+						<li>
+							<span>Max level: 75</span>
+							<ul>
+								<li><span>Instant lvl 61</span></li>
+								<li><span>Fast lvl 61-70</span></li>
+								<li><span>Fair medium lvl 70-75</span></li>
+							</ul>
+						</li>
+						<li>
+							<span>Max pet level: 25</span>
+							<ul>
+								<li><span>One level per minute</span></li>
+							</ul>
+						</li>
+						<li>
+							<span>Starter items pack</span>
+							<ul>
+								<li><span>Contains 100 bound gold, blue set level 50-60 +7, 
+									<br />enhanced weapons 60-70 +7, enhanced asistants</span></li>
+							</ul>
+						</li>
+						<li>
+							<span>Max color drop: golden</span>
+							<ul>
+								<li><span>Dark golden magic bead drop. (25% success rate)</span></li>
+							</ul>
+						</li>
+						<li>
+							<span>Fair larges and pills drop</span>
+							<ul>
+								<li><span>4-6 larges/pills in 100 mobs</span></li>
+							</ul>
+						</li>
+						<li>
+							<span>No EZ, QL, PB to avoid apprentices.</span>
+						</li>
+						<li>
+							<span>No PRC sets just yet</span>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	)
